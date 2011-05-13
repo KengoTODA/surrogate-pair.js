dab8967c3ddc7fece898f17d3d4bc912374ab136
eller86 <skypencil@gmail.com>
2011-05-14 08:56:46 +0900

    add scripts and test cases

---
+++ compress/compress.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/usr/bin/java -jar ./yuicompressor-2.4.6.jar ../script/surrogate-pair.js -o ../script/surrogate-pair.min.js
---
+++ script/surrogate-pair.js
@@ -0,0 +1,57 @@
+/*!
+ * surrogate-pair.js
+ * https://github.com/eller86/surrogate-pair.js
+ * 
+ * Copyright(c) 2011 eller86
+ * Licensed under Apache License Version 2.0
+ */
+;(function($){
+	var MIN_HIGH_SURROGATE = 0xD800, MAX_HIGH_SURROGATE = 0xDBFF,
+	    MIN_LOW_SURROGATE  = 0xDC00, MAX_LOW_SURROGATE  = 0xDFFF,
+	    PATTERN = /[\uD800-\uDBFF][\uDC00-\uDFFF]/;
+	function isString(obj) {
+		return typeof obj === 'string';
+	};
+	function isHighSurrogate(c) {
+		return c >= MIN_HIGH_SURROGATE && c <= MAX_HIGH_SURROGATE;
+	};
+	function isLowSurrogate(c) {
+		return c >= MIN_LOW_SURROGATE && c <= MAX_LOW_SURROGATE;
+	};
+
+	$.sp = {
+		codePointCount: function(str) {
+			if (!isString(str)) return 0;
+			var count = 0;
+			for (var i = 0; i < str.length; ++i) {
+				if (isHighSurrogate(str.charCodeAt(i)) && isLowSurrogate(str.charCodeAt(i + 1))) {
+					++i;
+				}
+				++count;
+			}
+			return count;
+		},
+		substr: function(str, startCodePoints, codePoints) {
+			if (!isString(str)) return '';
+			if (startCodePoints === undefined) { startCodePoints = 0; }
+			if (codePoints === undefined) { codePoints = str.length; }
+
+			var count = 0, startIndex = 0, endIndex = str.length;
+			for (var i = 0; i <= str.length; ++i) {
+				if (count === startCodePoints) { startIndex = i; }
+				if (count === startCodePoints + codePoints) { endIndex = i; break; }
+				if (isHighSurrogate(str.charCodeAt(i)) && isLowSurrogate(str.charCodeAt(i + 1))) {
+					++i;
+				}
+				++count;
+			}
+			return str.substr(startIndex, endIndex - startIndex);
+		},
+		containsSurrogatePair: function(str) {
+//			if (!isString(str)) return false;
+			return PATTERN.test(str);
+		},
+		isHighSurrogate: isHighSurrogate,
+		isLowSurrogate:  isLowSurrogate
+	};
+})(jQuery);
