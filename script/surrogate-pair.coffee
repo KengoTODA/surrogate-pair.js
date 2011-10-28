6a37383d3fdb8a72a15b8d37a7043b898f139b2c
eller86 <skypencil@gmail.com>
2011-10-28 23:41:42 +0800

    rewrite with coffeescript. Test is green on Chrome,Safari,Firefox and Opera.

---
+++ script/surrogate-pair.coffee
@@ -0,0 +1,53 @@
+###!
+surrogate-pair.js
+https://github.com/eller86/surrogate-pair.js
+
+Copyright(c) 2011 eller86
+Licensed under Apache License Version 2.0
+###
+MIN_HIGH_SURROGATE = 0xD800
+MAX_HIGH_SURROGATE = 0xDBFF
+MIN_LOW_SURROGATE  = 0xDC00
+MAX_LOW_SURROGATE  = 0xDFFF
+PATTERN = /[\uD800-\uDBFF][\uDC00-\uDFFF]/
+
+checkString = (obj) ->
+  typeof obj == 'string'
+
+checkHighSurrogate = (c) -> 
+  MIN_HIGH_SURROGATE <= c <= MAX_HIGH_SURROGATE
+
+checkLowSurrogate = (c) ->
+  MIN_LOW_SURROGATE <= c <= MAX_LOW_SURROGATE
+
+window.sp = {
+  countCodePoint: (string) ->
+    return 0 if !checkString(string)
+    count = 0
+    for i in [0...string.length]
+      ++i if checkHighSurrogate(string.charCodeAt(i)) && checkLowSurrogate(string.charCodeAt(i + 1))
+      ++count
+
+    return count
+
+  substr: (string='', startCodePoints=0, codePoints=string.length) ->
+    return '' if !checkString(string) || codePoints <= 0
+    count = 0
+    startIndex = 0
+    endIndex = string.length
+    for i in [0..string.length]
+      startIndex = i if count == startCodePoints
+      if count == startCodePoints + codePoints
+        endIndex = i
+        break
+      ++i if checkHighSurrogate(string.charCodeAt(i))&& checkLowSurrogate(string.charCodeAt(i + 1))
+      ++count
+    return string.substr(startIndex, endIndex - startIndex)
+
+  findSurrogatePair: (string) ->
+    PATTERN.test(string)
+
+  checkHighSurrogate: checkHighSurrogate
+  checkLowSurrogate: checkLowSurrogate
+
+}
--- script/surrogate-pair.js
+++ script/surrogate-pair.js
@@ -1,65 +1,77 @@
-/*!
- * surrogate-pair.js
- * https://github.com/eller86/surrogate-pair.js
- * 
- * Copyright(c) 2011 eller86
- * Licensed under Apache License Version 2.0
- */
-;(function(){
-	var MIN_HIGH_SURROGATE = 0xD800, MAX_HIGH_SURROGATE = 0xDBFF,
-	    MIN_LOW_SURROGATE  = 0xDC00, MAX_LOW_SURROGATE  = 0xDFFF,
-	    PATTERN = /[\uD800-\uDBFF][\uDC00-\uDFFF]/;
-	function isString(obj) {
-		return typeof obj === 'string';
-	};
-	function isHighSurrogate(c) {
-		return c >= MIN_HIGH_SURROGATE && c <= MAX_HIGH_SURROGATE;
-	};
-	function isLowSurrogate(c) {
-		return c >= MIN_LOW_SURROGATE && c <= MAX_LOW_SURROGATE;
-	};
-
-	sp = {
-		countCodePoint: function(str) {
-			if (!isString(str)) return 0;
-			var count = 0;
-			for (var i = 0; i < str.length; ++i) {
-				if (isHighSurrogate(str.charCodeAt(i)) && isLowSurrogate(str.charCodeAt(i + 1))) {
-					++i;
-				}
-				++count;
-			}
-			return count;
-		},
-		substr: function(str, startCodePoints, codePoints) {
-			if (!isString(str) || codePoints <= 0) return '';
-			if (startCodePoints === undefined) { startCodePoints = 0; }
-			var codePoints_ = codePoints;
-			if (codePoints === undefined) { codePoints = str.length; }
-
-			var count = 0, startIndex = 0, endIndex = str.length;
-			for (var i = 0; i <= str.length; ++i) {
-				if (count === startCodePoints) { startIndex = i; }
-				if (count === startCodePoints + codePoints) { endIndex = i; break; }
-				if (isHighSurrogate(str.charCodeAt(i)) && isLowSurrogate(str.charCodeAt(i + 1))) {
-					++i;
-				}
-				++count;
-			}
-			if (arguments.length < 3) {
-				return str.substr(startIndex);
-			} else if (codePoints_ === undefined) {
-				return str.substr(startIndex, undefined);
-			} else {
-				return str.substr(startIndex, endIndex - startIndex);
-			}
-		},
-		containsSurrogatePair: function(str) {
-			return PATTERN.test(str);
-		},
-		// TODO rename to good one do not like 'is~'
-		isHighSurrogate: isHighSurrogate,
-		// TODO rename to good one do not like 'is~'
-		isLowSurrogate:  isLowSurrogate
-	};
-})();
+(function() {
+  /*!
+  surrogate-pair.js
+  https://github.com/eller86/surrogate-pair.js
+  
+  Copyright(c) 2011 eller86
+  Licensed under Apache License Version 2.0
+  */
+  var MAX_HIGH_SURROGATE, MAX_LOW_SURROGATE, MIN_HIGH_SURROGATE, MIN_LOW_SURROGATE, PATTERN, checkHighSurrogate, checkLowSurrogate, checkString;
+  MIN_HIGH_SURROGATE = 0xD800;
+  MAX_HIGH_SURROGATE = 0xDBFF;
+  MIN_LOW_SURROGATE = 0xDC00;
+  MAX_LOW_SURROGATE = 0xDFFF;
+  PATTERN = /[\uD800-\uDBFF][\uDC00-\uDFFF]/;
+  checkString = function(obj) {
+    return typeof obj === 'string';
+  };
+  checkHighSurrogate = function(c) {
+    return (MIN_HIGH_SURROGATE <= c && c <= MAX_HIGH_SURROGATE);
+  };
+  checkLowSurrogate = function(c) {
+    return (MIN_LOW_SURROGATE <= c && c <= MAX_LOW_SURROGATE);
+  };
+  window.sp = {
+    countCodePoint: function(string) {
+      var count, i, _ref;
+      if (!checkString(string)) {
+        return 0;
+      }
+      count = 0;
+      for (i = 0, _ref = string.length; 0 <= _ref ? i < _ref : i > _ref; 0 <= _ref ? i++ : i--) {
+        if (checkHighSurrogate(string.charCodeAt(i)) && checkLowSurrogate(string.charCodeAt(i + 1))) {
+          ++i;
+        }
+        ++count;
+      }
+      return count;
+    },
+    substr: function(string, startCodePoints, codePoints) {
+      var count, endIndex, i, startIndex, _ref;
+      if (string == null) {
+        string = '';
+      }
+      if (startCodePoints == null) {
+        startCodePoints = 0;
+      }
+      if (codePoints == null) {
+        codePoints = string.length;
+      }
+      if (!checkString(string) || codePoints <= 0) {
+        return '';
+      }
+      count = 0;
+      startIndex = 0;
+      endIndex = string.length;
+      for (i = 0, _ref = string.length; 0 <= _ref ? i <= _ref : i >= _ref; 0 <= _ref ? i++ : i--) {
+        if (count === startCodePoints) {
+          startIndex = i;
+        }
+        if (count === startCodePoints + codePoints) {
+          endIndex = i;
+          break;
+        }
+        if (checkHighSurrogate(string.charCodeAt(i)) && checkLowSurrogate(string.charCodeAt(i + 1))) {
+          ++i;
+        }
+        ++count;
+      }
+      return string.substr(startIndex, endIndex - startIndex);
+    },
+    findSurrogatePair: function(string) {
+      return PATTERN.test(string);
+    },
+    checkHighSurrogate: checkHighSurrogate,
+    checkLowSurrogate: checkLowSurrogate
+  };
+}).call(this);
